086427fdf165b61a69139211f9a5319ddc3f37c1
Vedat Kara <vedatkara@mail.com>
2023-04-07 15:58:16 +0300

    MedOf3 debugged. Done.

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class QuickSort {
     /*
-    Quick-Sort is another divide and conquer algorithm like merge-sort. This algorithm uses pivots to divide the array
+    Quick-Sort is another divide and conquer algorithm like merge-sort. This algorithm uses pivot to divide the array
     that wanted to be sorted. Once pivot is selected, reindex the pivot to the correct position where elements
     at left of the pivot are smaller than pivot and elements at right of the pivot are bigger than pivot. After that
     array partition occurs. Then each sub-array repeats previous steps until no further division can be done.
@@ -10,13 +11,13 @@ public class QuickSort {
 
     public void quickSort(int[] arrayToSort, String pivotType) {
         if (pivotType.equals("FirstElement"))
-            quickSortFirst(arrayToSort, 0, arrayToSort.length -1);
+            quickSortFirst(arrayToSort, 0, arrayToSort.length - 1);
 
         else if (pivotType.equals("RandomElement"))
-            quickSortRandom(arrayToSort, 0 , arrayToSort.length -1);
+            quickSortRandom(arrayToSort, 0, arrayToSort.length - 1);
 
         else if (pivotType.equals("MidOfFirstMidLastElement")) {
-            quickSortMedian(arrayToSort, 0, arrayToSort.length-1);
+            quickSortMedian(arrayToSort, 0, arrayToSort.length - 1);
         } else
             System.out.println("'pivotType' parameter has wrong value.");
 
@@ -28,8 +29,7 @@ public class QuickSort {
         arr[j] = temp;
     }
 
-    private void random(int[] arrayToSort, int left, int right)
-    {
+    private void random(int[] arrayToSort, int left, int right) {
         Random rnd = new Random();
         // select random pivot between range of left and right(both included.)
         int piv = rnd.nextInt(right - left) + left;
@@ -38,20 +38,56 @@ public class QuickSort {
         swap(arrayToSort, piv, right);
     }
 
-    private int medianOf3(int[] arrayToSort, int left, int right){
-        int mid = (right - left) / 2; // find middle index
-
-        // order first, mid, last element the smallest to the biggest, find median.
-        if(arrayToSort[left] > arrayToSort[mid])
-            swap(arrayToSort, left, mid);
-        if(arrayToSort[left] > arrayToSort[right])
-            swap(arrayToSort, left, right);
-        if(arrayToSort[mid] > arrayToSort[right])
-            swap(arrayToSort, mid, right);
-
-        swap(arrayToSort, mid, right); // put the pivot(median) into the last index
-        return arrayToSort[right];
+    public long medianOf3(int[]arrayToSort, int left, int right) {
+        int mid = (left + right) / 2;
+        if (arrayToSort[left] > arrayToSort[mid])
+            swap(arrayToSort,left, mid);
+        if (arrayToSort[left] > arrayToSort[right])
+            swap(arrayToSort,left, right);
+        if (arrayToSort[mid] > arrayToSort[right])
+            swap(arrayToSort,mid, right);
+        swap(arrayToSort,mid, right - 1);    // move the pivot to right
+        return arrayToSort[right - 1];     // return pointer to location of median
     }
+
+    public int partitionMedian(int[] arrayToSort ,int left, int right, long pivot) {
+        int l = left; // index of the fist element
+        int r = right - 1; // pivot index's left
+
+        while (true) {
+            // scan to find elements smaller than pivot
+            while (arrayToSort[++l] < pivot);
+            // scan to find elements bigger than pivot
+            while (arrayToSort[--r] > pivot);
+            if (l >= r) // when two pointers cross each other break
+                break;
+            else
+                swap(arrayToSort,l, r); // if pointers did not cross swap the elements
+        }
+        swap(arrayToSort, l, right - 1); // put pivot into its correct position.
+        return l; // return the index where the pivot is.
+    }
+
+    public void simpleSort(int[] arrayToSort,int left, int right) {
+        int size = right - left + 1; // find size of the array.
+
+        if (size <= 1)
+            return; // if array has 1 element do nothing.
+        if (size == 2) {
+            // if array has only 2 elements compare and sort them
+            if (arrayToSort[left] > arrayToSort[right])
+                swap(arrayToSort,left, right);
+        } else {
+            // if array has only 3 elements no need to find median just sort.
+            if (arrayToSort[left] > arrayToSort[right - 1])
+                swap(arrayToSort,left, right - 1);
+            if (arrayToSort[left] > arrayToSort[right])
+                swap(arrayToSort,left, right);
+            if (arrayToSort[right - 1] > arrayToSort[right])
+                swap(arrayToSort,right - 1, right);
+        }
+    }
+
 
     // Divide the array as pivot's left and right.
     private int partitionFirstElement(int[] arr, int left, int right) {
@@ -64,7 +100,7 @@ public class QuickSort {
 
         // loop to find correct position of the pivot also reordering the array such way elements at left of the pivot
         // are smaller and elements at right of the pivot are bigger than pivot.
-        for (int j = right; j > left ; j--) {
+        for (int j = right; j > left; j--) {
 
             // If element at jth index (current) is bigger than pivot enters
             if (arr[j] > pivot) {
@@ -92,84 +128,77 @@ public class QuickSort {
     /*
     This function take pivot as last element (chooses pivot randomly and replace it to the last element.)
      */
-    private int partitionRandom(int[] arrayToSort, int left, int right)
-    {
+    private int partitionRandom(int[] arrayToSort, int left, int right) {
         // Chose pivot randomly
-        random(arrayToSort,left,right);
+        random(arrayToSort, left, right);
         int pivot = arrayToSort[right]; // Initialize the pivot.
 
         // index of the bigger element, also indicates correct position of the pivot.
-        int i = (left-1);
+        int i = (left - 1);
 
         // loop to find correct position of the pivot also reordering the array such way elements at left of the pivot
         // are smaller and elements at right of the pivot are bigger than pivot.
-        for (int j = left; j < right; j++)
-        {
+        for (int j = left; j < right; j++) {
             // If element at jth index (current) is smaller than pivot enters
-            if (arrayToSort[j] < pivot)
-            {
+            if (arrayToSort[j] < pivot) {
                 i++; // increase index of the smaller element
-                swap(arrayToSort, i,j); // swap the elements
+                swap(arrayToSort, i, j); // swap the elements
             }
         }
         // leave the loop when reaching to the pivot.
 
-        swap(arrayToSort, i+1, right); // place pivot into the correct index.
-        return i+1;
+        swap(arrayToSort, i + 1, right); // place pivot into the correct index.
+        return i + 1;
     }
 
 
-    private void quickSortRandom(int[] arrayToSort, int left, int right)
-    {
-        if (left < right)
-        {
+    private void quickSortRandom(int[] arrayToSort, int left, int right) {
+        if (left < right) {
             // Partition of the array, returns pivot's position.
             int part = partitionRandom(arrayToSort, left, right);
 
             // Recursive call for partition and quick-sorting.
-            quickSortRandom(arrayToSort, left, part-1);
-            quickSortRandom(arrayToSort, part+1, right);
+            quickSortRandom(arrayToSort, left, part - 1);
+            quickSortRandom(arrayToSort, part + 1, right);
         }
     }
 
-    // Divide the array as pivot's left and right.
-    private int partitionMedian(int[] arrayToSort, int left, int right, int pivot)
-    {
-        // index of the bigger element, also indicates correct position of the pivot.
-        int i = (left - 1);
-        int j = right;
+//    private int partitionMidElement(int[] arrayToSort, int left, int right){
+//        int i = left, j = right;
+//        int pivot = arrayToSort[(left + right) / 2];
+//
+//        while (i <= j) {
+//            while (arrayToSort[i] < pivot)
+//                i++;
+//            while (arrayToSort[j] > pivot)
+//                j--;
+//            if (i <= j) {
+//                swap(arrayToSort, i, j);
+//                i++;
+//                j--;
+//            }
+//
+//        };
+//        return i;
+//    }
 
-        while(i < j){
-            while(arrayToSort[++i] < pivot){
-                while(j > 0  && arrayToSort[--j] > pivot){
-                    if(i >= j)
-                        break;
-                    else
-                        swap(arrayToSort, i, j);
-                }
-            }
-        }
 
-        swap(arrayToSort, i, right); // place pivot into the correct index.
-        return i;
-    }
-
-    private void quickSortMedian(int[] arrayToSort, int left, int right)
-    {
-        if (left < right)
-        {
-            // Partition of the array, returns pivot's position.
-            int pivot = medianOf3(arrayToSort, left, right);
-            int part = partitionMedian(arrayToSort, left, right, pivot);
+    public void quickSortMedian(int[] arrayToSort, int left, int right) {
+        int size = right - left + 1;
+        // if size of the array bigger than 3, we can find median and make it pivot.
+        if (size >= 3) {
+            long median = medianOf3(arrayToSort,left, right);    // find the median and determine partition
+            int partition = partitionMedian(arrayToSort,left, right, median);
 
             // Recursive call for partition and quick-sorting.
-            quickSortMedian(arrayToSort, left, part-1);
-            quickSortMedian(arrayToSort, part+1, right);
+            quickSortMedian(arrayToSort,left, partition - 1);
+            quickSortMedian(arrayToSort,partition + 1, right);
+        }
+        else {
+            // if array size is ess than three just do simple sort.
+            simpleSort(arrayToSort,left, right);
         }
     }
-
-
-
 
 
 }
--- src/SortingClass.java
+++ src/SortingClass.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class SortingClass {
@@ -6,18 +5,22 @@ public class SortingClass {
     public static void main(String[] args) {
         // T = thousand TT = ten thousand, HT = hundred thousand.
         // Create and initialize arrays.
-        int[] arr = {7, 6, 10, 5, 9, 15, 2};
         int[] arrT = new int[1000], arrTT = new int[10000], arrHT = new int[100000];
         long start, est;
         MergeSort mergeSortObj = new MergeSort();
         QuickSort quickSortObj = new QuickSort();
 
-        //randomIntegers(arrT, 1000);
+        /*
+        For testing the sort methods with custom array.
+         */
+        int[] arr = {7, 6, 10, 5, 9, 15, 2};
         System.out.println(Arrays.toString(arr));
-        quickSortObj.quickSort(arr, "MidOfFirstMidLastElement");
+        mergeSortObj.mergeSort(arr, 3);
+        //quickSortObj.quickSort(arr, "MidOfFirstMidLastElement");
         System.out.println(Arrays.toString(arr));
 
         /* Merge Sort Two Parts */
+//        System.out.println();
 //        System.out.println("-------------------------Merge Sort Two Way-------------------------");
 //        equalIntegers(arrT, 1000);
 //        start = System.currentTimeMillis();
@@ -313,40 +316,102 @@ public class SortingClass {
 //        est = System.currentTimeMillis() - start;
 //        System.out.println("Decreasing 100.000 | " + est + "ms\n");
 
+//        System.out.println("-------------------------Quick Sort MedOf3-------------------------");
+//        equalIntegers(arrT, 1000);
+//        start = System.currentTimeMillis();
+//        quickSortObj.quickSort(arrT, "MidOfFirstMidLastElement");
+//        est = System.currentTimeMillis() - start;
+//        System.out.println("Equal 1.000 | " + est + "ms");
+//
+//        equalIntegers(arrTT, 10000);
+//        start = System.currentTimeMillis();
+//        quickSortObj.quickSort(arrTT, "MidOfFirstMidLastElement");
+//        est = System.currentTimeMillis() - start;
+//        System.out.println("Equal 10.000 | " + est + "ms");
+//
+//        equalIntegers(arrHT, 100000);
+//        start = System.currentTimeMillis();
+//        quickSortObj.quickSort(arrHT, "MidOfFirstMidLastElement");
+//        est = System.currentTimeMillis() - start;
+//        System.out.println("Equal 100.000 | " + est + "ms\n");
+//
+//        randomIntegers(arrT, 1000);
+//        start = System.currentTimeMillis();
+//        quickSortObj.quickSort(arrT, "MidOfFirstMidLastElement");
+//        est = System.currentTimeMillis() - start;
+//        System.out.println("Random 1.000 | " + est + "ms");
+//
+//        randomIntegers(arrTT, 10000);
+//        start = System.currentTimeMillis();
+//        quickSortObj.quickSort(arrTT, "MidOfFirstMidLastElement");
+//        est = System.currentTimeMillis() - start;
+//        System.out.println("Random 10.000 | " + est + "ms");
+//
+//        randomIntegers(arrHT, 100000);
+//        start = System.currentTimeMillis();
+//        quickSortObj.quickSort(arrHT, "MidOfFirstMidLastElement");
+//        est = System.currentTimeMillis() - start;
+//        System.out.println("Random 100.000 | " + est + "ms\n");
+//
+//        increasingIntegers(arrT, 1000);
+//        start = System.currentTimeMillis();
+//        quickSortObj.quickSort(arrT, "MidOfFirstMidLastElement");
+//        est = System.currentTimeMillis() - start;
+//        System.out.println("Increasing 1.000 | " + est + "ms");
+//
+//        increasingIntegers(arrTT, 10000);
+//        start = System.currentTimeMillis();
+//        quickSortObj.quickSort(arrTT, "MidOfFirstMidLastElement");
+//        est = System.currentTimeMillis() - start;
+//        System.out.println("Increasing 10.000 | " + est + "ms");
+//
+//        increasingIntegers(arrHT, 100000);
+//        start = System.currentTimeMillis();
+//        quickSortObj.quickSort(arrHT, "MidOfFirstMidLastElement");
+//        est = System.currentTimeMillis() - start;
+//        System.out.println("Increasing 100.000 | " + est + "ms\n");
+//
+//        decreasingIntegers(arrT, 1000);
+//        start = System.currentTimeMillis();
+//        quickSortObj.quickSort(arrT, "MidOfFirstMidLastElement");
+//        est = System.currentTimeMillis() - start;
+//        System.out.println("Decreasing 1.000 | " + est + "ms");
+//
+//        decreasingIntegers(arrTT, 10000);
+//        start = System.currentTimeMillis();
+//        quickSortObj.quickSort(arrTT, "MidOfFirstMidLastElement");
+//        est = System.currentTimeMillis() - start;
+//        System.out.println("Decreasing 10.000 | " + est + "ms");
+//
+//        decreasingIntegers(arrHT, 100000);
+//        start = System.currentTimeMillis();
+//        quickSortObj.quickSort(arrHT, "MidOfFirstMidLastElement");
+//        est = System.currentTimeMillis() - start;
+//        System.out.println("Decreasing 100.000 | " + est + "ms\n");
     }
 
-    public static void equalIntegers(int[] arr, int numOfInt){
-        for(int i = 0; i < numOfInt; i++)
+    public static void equalIntegers(int[] arr, int numOfInt) {
+        for (int i = 0; i < numOfInt; i++)
             arr[i] = 5;
     }
 
-    public static void randomIntegers(int[] arr, int numOfInt){
+    public static void randomIntegers(int[] arr, int numOfInt) {
         int rnd;
-        for(int i = 0; i < numOfInt; i++){
+        for (int i = 0; i < numOfInt; i++) {
             rnd = (int) (Math.random() * numOfInt);
             arr[i] = rnd;
         }
     }
 
-    public static void increasingIntegers(int[] arr, int numOfInt){
-        for(int i = 0; i < numOfInt; i++)
+    public static void increasingIntegers(int[] arr, int numOfInt) {
+        for (int i = 0; i < numOfInt; i++)
             arr[i] = i;
     }
 
-    public static void decreasingIntegers(int[] arr, int numOfInt){
-        for(int i = numOfInt - 1; i >= 0; i--)
+    public static void decreasingIntegers(int[] arr, int numOfInt) {
+        for (int i = numOfInt - 1; i >= 0; i--)
             arr[i] = i;
     }
-
-
-
-
-
-
-
-
-
-
 
 
 }
